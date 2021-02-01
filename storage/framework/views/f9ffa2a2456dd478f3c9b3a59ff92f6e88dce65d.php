c97da16444d946da91a44c21ad3e97a45e674dfd
--A-- <32413982+es9d@users.noreply.github.com>
2021-02-01 20:10:11 +0100

    contact page header

---
+++ storage/framework/views/f9ffa2a2456dd478f3c9b3a59ff92f6e88dce65d.php
@@ -0,0 +1,11 @@
+<div class="block_contact">
+    <div class="c-header-top">
+        THE BEST FREELANCER<br>
+        NETWORK
+    </div>
+    <div class="c-header-main-text">
+        <h1 class="main-header-h1">Contact Työkokeilu</h1>
+        <p class="main-header-p">Where the world meets startups. Thousands of small businesses use<br>Työkokeilu to turn their ideas into reality</p>
+    </div>
+</div>
+<?php /**PATH C:\xampp\htdocs\modules/Contact/Views/frontend/blocks/contact/center.blade.php ENDPATH**/ ?>
