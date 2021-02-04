c57db8db492d2a11e33104f5cdcc5c7742fa732d
--A-- <32413982+es9d@users.noreply.github.com>
2021-02-04 16:24:38 +0100

    ??

--- storage/framework/views/0950a9d142e02e6cc028a6be84d2a00a50b3f81c.php
+++
@@ -1,1 +0,0 @@
-<?php echo $__env->make("Layout::user", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\resources\views/layouts/user.blade.php ENDPATH**/ ?>
--- storage/framework/views/13f5010e7b789888c7da4bdac6853e567029f911.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <div class="bravo-text <?php echo e($class ?? ''); ?>">
-        <?php echo clean($content ?? ''); ?>
-    </div>
-</div><?php /**PATH C:\xampp\htdocs\modules/Template/Views/frontend/blocks/text.blade.php ENDPATH**/ ?>
