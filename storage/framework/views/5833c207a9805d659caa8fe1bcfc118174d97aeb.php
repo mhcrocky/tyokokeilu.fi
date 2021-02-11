2f92198903a88a0cac971651c6b5897a4565a1be
--A-- <32413982+es9d@users.noreply.github.com>
2021-02-11 12:20:32 +0100

    remove cache

--- storage/framework/views/5833c207a9805d659caa8fe1bcfc118174d97aeb.php
+++
@@ -1,8 +0,0 @@
-<?php if($row->map_lat && $row->map_lng): ?>
-    <div class="g-location">
-        <div class="location-map">
-            <div id="map_content"></div>
-        </div>
-    </div>
-<?php endif; ?>
-<?php /**PATH C:\xampp\htdocs\modules/Location/Views/frontend/layouts/details/location-map.blade.php ENDPATH**/ ?>
