ed15e567a25dcd6ff56e4ff52b977c3461aee922
pcstar0217 <kjav.acc@gmail.com>
2021-06-08 09:52:16 +0300

    cache

---
+++ storage/framework/views/689604692341b4079f5ff2a8a5768786bdad3745.php
@@ -0,0 +1,31 @@
+<div class="bravo_company">
+	<div class="container">
+		<div class="heading">Companies we’ve helped to hire the best candidate</div>
+		<div class="content row">
+			<div class="col-md-5" style="background-image: url(images/company.png); background-size:100% 100%; height:478px;"></div>
+			<div class="col-md-7 left">
+				<p>We have end-to-end solutions that can keep up with</p>
+				<p>you and your standards.</p>
+				<a href="#" class="btn btn-danger">Post a Job</a>
+			</div>
+		</div>
+		<div class="cards text-center">
+			<div class="card-item float-left">
+				<div style="background: url(images/Uber.svg); background-size:100% 100%; height:36px;"></div>
+				<div class="mt-5">We have end-to-end solutions that can keep up with you and your standards.</div>
+			</div>
+			<div class="card-item ml-5 float-left">
+				<div style="padding: 0px 40px 0px 40px">
+					<div style=" background-color: #88C129; height:36px;"></div>
+				</div>
+				<div class="mt-5">We hav end-to-end solutions that can keep up with you and your standards.</div>
+			</div>
+			<div class="card-item ml-5 float-left">
+				<div style="padding: 0px 30px 0px 30px">
+					<div style="height:36px; background-image: url(images/airbnb.svg);background-size:100% 100%; " ></div>
+				</div>
+				<div class="mt-5">We have end-to-end solutions that can keep up with you and your standards.</div>
+			</div>
+		</div>
+	</div>
+</div><?php /**PATH D:\Task\2021-05-08(Vargar@250$)\modules/Layout/parts/company.blade.php ENDPATH**/ ?>
